156db120ff0901409c3309c177e776c5bfb00be2
Wang Jing <wangjild@gmail.com>
2018-02-27 12:30:55 +0800

    fix bugs
    Change-Id: I2f6b0f9b358bdf590ebc1ced00a11d97636fce1a

--- octopus/wsgi.py
+++ octopus/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+from octopus.setup import prepare_env
+prepare_env()
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "octopus.settings")
